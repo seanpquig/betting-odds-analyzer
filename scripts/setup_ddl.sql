84835d0ff755f31c346e2690c134e05eb97d49e1
squigley <squigley@shutterstock.com>
2015-12-23 16:24:08 -0500

    add MySQL DDL to setup basic database to build upon

---
+++ scripts/setup_ddl.sql
@@ -0,0 +1,24 @@
+-- Create MMA database
+CREATE DATABASE IF NOT EXISTS mma_betting_db;
+
+-- Create athletes table
+CREATE TABLE IF NOT EXISTS mma_betting_db.athletes (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(128) NOT NULL,
+  lastname VARCHAR(128) NOT NULL,
+  nickname VARCHAR(128),
+  birthday DATE,
+  height_cm DOUBLE,
+  weight_kg DOUBLE
+);
+
+-- Create fights table
+CREATE TABLE IF NOT EXISTS mma_betting_db.fights (
+  id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  event_date DATE NOT NULL,
+  athlete1_id INT UNSIGNED NOT NULL,
+  athlete2_id INT UNSIGNED NOT NULL,
+
+  FOREIGN KEY (athlete1_id) REFERENCES athletes(id),
+  FOREIGN KEY (athlete2_id) REFERENCES athletes(id)
+);
