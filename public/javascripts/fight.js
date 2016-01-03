3aa254e3b7f1935932793aa1c00eacd24778b4be
squigley <squigley@shutterstock.com>
2016-01-03 04:07:24 -0500

    clean up front-end code, tweak CSS, and set up data for looking up athlete data for a fight

--- public/javascripts/fight.js
+++ public/javascripts/fight.js
@@ -2,7 +2,9 @@ if (window.console) {
     console.log("Welcome to your Play application's JavaScript!");
 }
 
-// Functions to support drag and drop functionality
+/****************************************************
+    Functions to support drag and drop functionality
+*****************************************************/
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -26,10 +28,19 @@ function multiplyNode(node, count, deep) {
 }
 multiplyNode(document.querySelector('.bet'), 5, true);
 
+
+/********************************************
+    Objects for tracking and re-using data 
+*********************************************/
 // object to map event_name => event_id
 var eventNameIdMap = {};
+// object for mapping fight_name => athletes data
+var fightToAthleteData = {};
 
-// On page-load => populate events drop down
+
+/************************************************
+    On page-load => populate events drop down
+*************************************************/
 $(document).ready(function() {
     var getEventsCall = jsRoutes.controllers.StatsDatabase.getEvents(1);
     $.getJSON(
@@ -44,16 +55,18 @@ $(document).ready(function() {
                 });
             }
             if(textStatus == "error") {
-                alert("Error getting events: " + xhr.status + ": " + xhr.statusText);
+                alert("Error for getEvents: " + xhr.status + ": " + xhr.statusText);
             }
         }
     );
 });
 
-// On event-selection => populate fights drop down
-function populateFights(fightName) {
-    var eventId = eventNameIdMap[fightName];
-    console.log("in populateFights! " + eventId);
+
+/***************************************************
+    On event-selection => populate fights drop down
+****************************************************/
+function populateFights(eventName) {
+    var eventId = eventNameIdMap[eventName];
     var getFightsCall = jsRoutes.controllers.StatsDatabase.getFights(eventId);
     $.getJSON(
         getFightsCall.url,
@@ -62,30 +75,43 @@ function populateFights(fightName) {
                 // Clear original list
                 $("#fights").empty();
                 $.each(data, function(index, value) {
-                     console.log("fight data: " + JSON.stringify(value));
-                     createFightOption(value["id"]);
+                     createFightOption(value["id"], value["athlete1_id"], value["athlete2_id"]);
                 });
             }
             if(textStatus == "error") {
-                alert("Error getting fights: " + xhr.status + ": " + xhr.statusText);
+                alert("Error for getFights: " + xhr.status + ": " + xhr.statusText);
             }
         }
     );
 }
-function createFightOption(fightId) {
+
+function createFightOption(fightId, athlete1Id, athlete2Id) {
     var getAthleteNamesCall = jsRoutes.controllers.StatsDatabase.getAthleteNames(fightId);
     $.getJSON(
         getAthleteNamesCall.url,
         function(data, textStatus, xhr) {
             if(textStatus == "success") {
+                var fightName = data["athlete1"]+ " vs. " + data["athlete2"];
+                fightToAthleteData[fightName] = {"athlete1_id": athlete1Id, "athlete2_id": athlete2Id};
+
                 // Create datalist option
                 $("#fights").append(
-                    "<option value='" + data["athlete1"]+ " vs. " + data["athlete2"] + "'></option>"
+                    "<option value='" + fightName + "'></option>"
                 );
             }
             if(textStatus == "error") {
-                alert("Error getting fights: " + xhr.status + ": " + xhr.statusText);
+                alert("Error for getAthleteNames: " + xhr.status + ": " + xhr.statusText);
             }
         }
     );
+}
+
+
+/***************************************************
+    Set fighter stats upon fight selection
+****************************************************/
+function populateAthleteStats(fightName) {
+    athlete1Id = fightToAthleteData[fightName]["athlete1_id"];
+    athlete2Id = fightToAthleteData[fightName]["athlete2_id"];
+    console.log("ids: " + athlete1Id + " " + athlete2Id);
 }
