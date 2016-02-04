ae2f813fac84ad982452f26d15ea184d13e3435a
squigley <squigley@shutterstock.com>
2016-02-04 01:03:44 -0500

    clean up spacing and formatting

--- public/javascripts/probability.scatterplot.js
+++ public/javascripts/probability.scatterplot.js
@@ -1,36 +1,36 @@
 // set the stage
-    var margin = {t:30, r:20, b:20, l:40 },
-        w = 600 - margin.l - margin.r,
-        h = 500 - margin.t - margin.b,
-        x = d3.scale.linear().range([0, w]),
-        y = d3.scale.linear().range([h - 60, 0]),
-        //colors that will reflect geographical regions
-        color = d3.scale.category10();
+var margin = {t:30, r:20, b:20, l:40 },
+    w = 600 - margin.l - margin.r,
+    h = 500 - margin.t - margin.b,
+    x = d3.scale.linear().range([0, w]),
+    y = d3.scale.linear().range([h - 60, 0]),
+    //colors that will reflect geographical regions
+    color = d3.scale.category10();
 
-    var svg = d3.select("#chart").append("svg")
-        .attr("width", w + margin.l + margin.r)
-        .attr("height", h + margin.t + margin.b);
+var svg = d3.select("#chart").append("svg")
+    .attr("width", w + margin.l + margin.r)
+    .attr("height", h + margin.t + margin.b);
 
     // set axes, as well as details on their ticks
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .ticks(20)
-        .tickSubdivide(true)
-        .tickSize(6, 3, 0)
-        .orient("bottom");
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .ticks(20)
+    .tickSubdivide(true)
+    .tickSize(6, 3, 0)
+    .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .ticks(20)
-        .tickSubdivide(true)
-        .tickSize(6, 3, 0)
-        .orient("left");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .ticks(20)
+    .tickSubdivide(true)
+    .tickSize(6, 3, 0)
+    .orient("left");
 
-    // group that will contain all of the plots
-    var groups = svg.append("g").attr("transform", "translate(" + margin.l + "," + margin.t + ")");
+// group that will contain all of the plots
+var groups = svg.append("g").attr("transform", "translate(" + margin.l + "," + margin.t + ")");
 
-    // array of the regions, used for the legend
-    var regions = ["Asia", "Europe", "Middle East", "N. America", "S. America", "Sub-Saharan Africa"];
+// array of the regions, used for the legend
+var regions = ["Asia", "Europe", "Middle East", "N. America", "S. America", "Sub-Saharan Africa"];
 
 
 // bring in the data, and do everything that is data-driven
@@ -45,33 +45,32 @@ d3.csv("assets/stylesheets/trust-business.csv", function(data) {
     y.domain([180, 0]);
 
     // style the circles, set their locations based on data
-    var circles =
-    groups.selectAll("circle")
+    var circles = groups.selectAll("circle")
         .data(data)
-      .enter().append("circle")
-      .attr("class", "circles")
-      .attr({
-        cx: function(d) { return x(+d.trust); },
-        cy: function(d) { return y(+d.business); },
-        r: 8,
-        id: function(d) { return d.country; }
-      })
+        .enter().append("circle")
+        .attr("class", "circles")
+        .attr({
+            cx: function(d) { return x(+d.trust); },
+            cy: function(d) { return y(+d.business); },
+            r: 8,
+            id: function(d) { return d.country; }
+        })
         .style("fill", function(d) { return color(d.region); });
 
     // what to do when we mouse over a bubble
     var mouseOn = function() {
         var circle = d3.select(this);
 
-    // transition to increase size/opacity of bubble
+        // transition to increase size/opacity of bubble
         circle.transition()
-        .duration(800).style("opacity", 1)
-        .attr("r", 16).ease("elastic");
+            .duration(800).style("opacity", 1)
+            .attr("r", 16).ease("elastic");
 
         // append lines to bubbles that will be used to show the precise data points.
         // translate their location based on margins
         svg.append("g")
             .attr("class", "guide")
-        .append("line")
+            .append("line")
             .attr("x1", circle.attr("cx"))
             .attr("x2", circle.attr("cx"))
             .attr("y1", +circle.attr("cy") + 26)
@@ -83,7 +82,7 @@ d3.csv("assets/stylesheets/trust-business.csv", function(data) {
 
         svg.append("g")
             .attr("class", "guide")
-        .append("line")
+            .append("line")
             .attr("x1", +circle.attr("cx") - 16)
             .attr("x2", 0)
             .attr("y1", circle.attr("cy"))
@@ -93,32 +92,39 @@ d3.csv("assets/stylesheets/trust-business.csv", function(data) {
             .transition().delay(200).duration(400).styleTween("opacity",
                         function() { return d3.interpolate(0, 0.5); });
 
-    // function to move mouseover item to front of SVG stage, in case
-    // another bubble overlaps it
+        // function to move mouseover item to front of SVG stage, in case
+        // another bubble overlaps it
         d3.selection.prototype.moveToFront = function() {
-          return this.each(function() {
-            this.parentNode.appendChild(this);
-          });
+            return this.each(function() {
+                this.parentNode.appendChild(this);
+            });
         };
 
-    // skip this functionality for IE9, which doesn't like it
+        // skip this functionality for IE9, which doesn't like it
         if (!$.browser.msie) {
             circle.moveToFront();
-            }
+        }
     };
+
     // what happens when we leave a bubble?
     var mouseOff = function() {
         var circle = d3.select(this);
 
         // go back to original size and opacity
         circle.transition()
-        .duration(800).style("opacity", 0.5)
-        .attr("r", 8).ease("elastic");
+            .duration(800).style("opacity", 0.5)
+            .attr("r", 8).ease("elastic");
 
         // fade out guide lines, then remove them
-        d3.selectAll(".guide").transition().duration(100).styleTween("opacity",
-                        function() { return d3.interpolate(0.5, 0); })
+        d3.selectAll(".guide").transition().duration(100)
+            .styleTween(
+                "opacity",
+                function() {
+                    return d3.interpolate(0.5, 0);
+                }
+            )
             .remove();
+
     };
 
     // run the mouseon/out functions
@@ -127,30 +133,30 @@ d3.csv("assets/stylesheets/trust-business.csv", function(data) {
 
     // tooltips (using jQuery plugin tipsy)
     circles.append("title")
-            .text(function(d) { return d.country; });
+        .text(function(d) { return d.country; });
 
     $(".circles").tipsy({ gravity: 's', });
 
     // the legend color guide
     var legend = svg.selectAll("rect")
-            .data(regions)
+        .data(regions)
         .enter().append("rect")
         .attr({
-          x: function(d, i) { return (40 + i*80); },
-          y: h,
-          width: 25,
-          height: 12
+            x: function(d, i) { return (40 + i*80); },
+            y: h,
+            width: 25,
+            height: 12
         })
         .style("fill", function(d) { return color(d); });
 
 
     // legend labels
-        svg.selectAll("text")
-            .data(regions)
+    svg.selectAll("text")
+        .data(regions)
         .enter().append("text")
         .attr({
-        x: function(d, i) { return (40 + i*80); },
-        y: h + 24,
+            x: function(d, i) { return (40 + i*80); },
+            y: h + 24
         })
         .text(function(d) { return d; });
 
@@ -180,4 +186,5 @@ d3.csv("assets/stylesheets/trust-business.csv", function(data) {
         .attr("dy", ".75em")
         .attr("transform", "rotate(-90)")
         .text("ease of doing business (rank)");
-    });
+
+});
